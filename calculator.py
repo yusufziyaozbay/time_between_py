de2e99b95816b5e1f180d76af4e7ed85d2edc2ad
yusufziyaozbay <yusufziyaozbay@gmail.com>
2023-05-29 17:03:24 +0300

    uploaded.

---
+++ calculator.py
@@ -0,0 +1,21 @@
+# Calculate the difference between two selected times
+
+# Get the user input
+time_input1 = input('Enter first time input (--:--): ')
+time_input2 = input('2nd input (--:--): ')
+
+# Split times to hour and minute
+time_input_parts1 = time_input1.split(':')
+time_input_parts2 = time_input2.split(':')
+
+time1_hour = int(time_input_parts1[0])
+time2_hour = int(time_input_parts2[0])
+
+time1_minute = int(time_input_parts1[1])
+time2_minute = int(time_input_parts2[1])
+
+# Calculate
+minute = time2_minute - time1_minute
+hour = time1_hour - time2_hour
+
+print(f'Time1 - Time2 = {hour}:{minute}')
